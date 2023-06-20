d933f8db49e544ef4ab0056c70b159a739c6956c
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-06-20 16:10:27 -0500

    BoardBuilder_Should_Build_ConsoleBoard_WithLength4

--- BoardTests.cs
+++ BoardTests.cs
@@ -15,12 +15,33 @@ namespace MemoryGameTests
         }
 
         [Test]
-        public void BoardBuilder_Should_Build_ConsoleBoard()
+        public void BoardBuilder_Should_Build_ConsoleBoard_WithLength2()
         {
             var board = new ConsoleBoardBuilder()
                 .WithDimensions(2, 2)
                 .Build();
             Assert.That(board.cells.Length, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void BoardBuilder_Should_Build_ConsoleBoard_WithLength4()
+        {
+            var board = new ConsoleBoardBuilder()
+                .WithDimensions(4, 2)
+                .Build();
+            Assert.That(board.cells.Length, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void BoardBuilder_Should_Build_ConsoleBoard_WithLength6()
+        {
+            var board = new ConsoleBoardBuilder()
+                .WithDimensions(4, 6)
+                .Build();
+            Assert.That(board.cells.Length, Is.EqualTo(4));
+            for (int i = 0; i < 4; i++)
+                Assert.That(board.cells[i].Length, Is.EqualTo(6));
+
         }
     }
 }
