b5208cb6f245581a326d42caa614dfb9c70e1e9a
Muratbek Ishenbaev <MIshenba@win.cccis.com>
2023-06-20 15:59:37 -0500

    init commit

---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+using MemoryGame;
+
+namespace MemoryGameTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Cell_SetImage_Should_Set_Valid_Image()
+        {
+            var cell = new Cell();
+            cell.SetImage("Image1");
+            Assert.That(cell.GetImage(), Is.EqualTo("Image1"));
+        }
+
+        [Test]
+        public void Cell_SetAsFound_True()
+        {
+            var cell = new Cell();
+            cell.SetAsFound();
+            Assert.That(cell.IsFound(), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Cell_SetAsFound_False()
+        {
+            var cell = new Cell();
+            Assert.That(cell.IsFound(), Is.EqualTo(false));
+        }
+    }
+}
