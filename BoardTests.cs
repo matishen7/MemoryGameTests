3072b39737f3e2aa3bb6b0c1ad66ba4ea57ee117
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-06-20 16:07:11 -0500

    BoardBuilder_Should_Build_ConsoleBoard

---
+++ BoardTests.cs
@@ -0,0 +1,26 @@
+﻿using MemoryGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGameTests
+{
+    public class BoardBuilderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BoardBuilder_Should_Build_ConsoleBoard()
+        {
+            var board = new ConsoleBoardBuilder()
+                .WithDimensions(2, 2)
+                .Build();
+            Assert.That(board.cells.Length, Is.EqualTo(2));
+        }
+    }
+}
